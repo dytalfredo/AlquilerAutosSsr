1ea5c21eaf2da7e65f41ae3be8acc96df05ac622
Alfredo <dytalfredomendoza@gmail.com>
2024-08-11 21:57:01 -0400

    firebase

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDHw7rjeFQhJlDcJ1MuCG6pnU02_i29WYE",
+    authDomain: "latamvisa-6e315.firebaseapp.com",
+    projectId: "latamvisa-6e315",
+    storageBucket: "latamvisa-6e315.appspot.com",
+    messagingSenderId: "756274202160",
+    appId: "1:756274202160:web:b243a6bcf362b75ab6cff5",
+    measurementId: "G-TM89D2LMYJ"
+};
+
+export const app = initializeApp(firebaseConfig);
